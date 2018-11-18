af242b3d66715e156844ab33400e077133c20d4b
coooopeeeer <miminashicooper@gmail.com>
2018-11-17 19:31:46 -0800

    Add script to hide modal

--- Modal/js/main.js
+++ Modal/js/main.js
@@ -2,13 +2,25 @@ let open = document.getElementById("open"); // open button
 let modal = document.getElementById("modalWindow"); // modal content
 let close = document.getElementById("close"); // close button
 
+// Function to show modal
 function show() {
     // Add background overlay when a modal is appeared
-    let bg = document.createElement("div");
+    var bg = document.createElement("div");
     bg.setAttribute("id", "overLay");
     document.body.append(bg);
     // Add style for modal
     modal.style.display = "block";
+    // Remove background overlay when a background is clicked
+    bg.addEventListener("click", hide);
+}
+
+// Function to hide modal
+function hide() {
+    // Remove background
+    bg.remove();
+    // Remove modal
+    modal.style.display = "none";
 }
 
 open.addEventListener("click", show);
+close.addEventListener("click", hide);
