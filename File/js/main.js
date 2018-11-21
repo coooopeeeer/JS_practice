22d2a89ebb370e90936b210ea16a59c4dc9735a8
coooopeeeer <miminashicooper@gmail.com>
2018-11-21 14:01:19 -0800

    Add script for dragover

--- File/js/main.js
+++ File/js/main.js
@@ -1,2 +1,12 @@
-let drop_area = document.getElementById("dropArea");
 let max_size = 20 * 1024 * 1024; // 20MB
+let drop_area = document.getElementById("dropArea");
+let file_list = document.getElementById("fileList");
+
+// Function while the file covers the area
+function overEvent(e) {
+    drop_area.style.backgroundColor = "ddd";
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+}
+
+drop_area.addEventListener("dragover", overEvent);
